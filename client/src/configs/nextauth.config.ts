fd4640f98a7af7eb52a358c2bf15179ca0c02036
hydra07 <abcxyzkhong@gmail.com>
2024-09-23 10:58:01 +0700

    init project

---
+++ client/src/configs/nextauth.config.ts
@@ -0,0 +1,25 @@
+import { AuthOptions } from 'next-auth';
+import Google from 'next-auth/providers/google';
+
+const authOptions: AuthOptions = {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    maxAge: 2 * 60 * 60,
+  },
+  callbacks: {
+    async signIn({user,account,profile,email,credentials ){
+      if (!account) return false;
+      return true
+    }
+  }
+};
+
+
+//Auth service 
+const 
