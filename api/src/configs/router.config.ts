4159c026b92657e72f78ba7df834afcbec98ce41
Hoang Enzo <96958365+hoangenzo11@users.noreply.github.com>
2024-10-13 18:44:28 +0700

    fix rating and comments

--- api/src/configs/router.config.ts
+++ api/src/configs/router.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import type { Router } from "express";
 import authRouter from "../routers/auth.router";
 import fileRouter from "../routers/file.router";
@@ -7,16 +6,6 @@ import commentRouter from "../routers/comment.router"; // Import the comment rou
 import userRouter from "../routers/user.router"; // Import the user router
 import ratingRouter from "../routers/rating.router";
 import settingRouter from "../routers/setting.router";
-=======
-import type { Router } from 'express';
-import authRouter from '../routers/auth.router'
-import bookRouter from '../routers/book.router';
-import fileRouter from '../routers/file.router';
-
-import settingRouter from '../routers/setting.router';
-=======
-
->>>>>>> 324c8326c7e87f5da935b3cd1aedd83639d879b2
 interface IRouterConfig {
   path: string;
   router: Router;
@@ -48,13 +37,12 @@ export default [
     path: "/api/comments", // Add the path for comments
     router: commentRouter,
   },
-    {
-      path: "/api/users", // Add the path for users
-        router: userRouter,
-    },
-    {
-      path: "/api/ratings",
-        router: ratingRouter,
-    }
-
+  {
+    path: "/api/users", // Add the path for users
+    router: userRouter,
+  },
+  {
+    path: "/api/ratings",
+    router: ratingRouter,
+  },
 ] as IRouterConfig[];
