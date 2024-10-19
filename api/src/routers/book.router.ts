7261d3352f65ead531fe4ebcedefebd5fbc3eb3d
hnil206 <luundde170719@fpt.edu.vn>
2024-10-19 15:11:36 +0700

    before pull

--- api/src/routers/book.router.ts
+++ api/src/routers/book.router.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { adminAuthMiddleware } from '../middlewares/admin.middleware';
 import {
   createBook,
   getAllBooks,
@@ -8,17 +7,16 @@ import {
   deleteBook,
   searchBooks
 } from '../controller/book.controller';
-
+import roleRequire from '../configs/middleware.config';
 const router = express.Router();
 
 // Public routes
-router.get('/', getAllBooks);
-router.get('/search', searchBooks);
-router.get('/:id', getBookById);
-
+router.get('/',  getAllBooks);
+router.get('/search',   searchBooks);
+router.get('/:id',   getBookById);
 // Admin-only route
-router.post('/', createBook);
-router.put('/:id', updateBook);
-router.delete('/:id', deleteBook);
+router.post('/',     createBook);
+router.put('/:id',   updateBook);
+router.delete('/:id',   deleteBook);
 
 export default router;
